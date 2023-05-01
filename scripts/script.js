6f6efad1f897c8a174f0a7366f2a792de9456cc2
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-01 11:49:59 -0700

    Update files
    Added CSS and reset buttons

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,25 @@ window.onload = init;
 
 function init(){
     const calcMortgageBtn = document.getElementById("calcMortgageBtn");
+    const resetMortgageBtn = document.getElementById("resetMortgageBtn");
     const calcCDValueBtn = document.getElementById("calcCDValueBtn");
     const calcPresentValueBtn = document.getElementById("calcPresentValueBtn");
+    const resetCDBtn = document.getElementById("resetCDBtn");
+    const resetPVBtn = document.getElementById("resetPVBtn");
 
     if(calcMortgageBtn != null){
         calcMortgageBtn.onclick = onCalculateMortgageBtn;
+        resetMortgageBtn.onclick = onResetMortgageBtn;
     }
     else if (calcCDValueBtn != null){
         calcCDValueBtn.onclick = onCalculateCDValueBtn;
+        resetCDBtn.onclick = onResetCDBtn;
     }
     else if(calcPresentValueBtn != null){
-        calcPresentValueBtn.onclick = onCalculatePresentValueBtn;   
+        calcPresentValueBtn.onclick = onCalculatePresentValueBtn;
+        resetPVBtn.onclick = onResetPVBtn;
     }
+
 }
 
 function onCalculateMortgageBtn(){
@@ -34,17 +41,32 @@ function onCalculateMortgageBtn(){
 
 }
 
+function onResetMortgageBtn(){
+    document.getElementById("principalAmount").value = "";
+    document.getElementById("interestRate").value = "";
+    document.getElementById("loanLength").value = "";
+    document.getElementById("monthlyPay").value = "";
+    document.getElementById("totalInterest").value = "";
+}
+
 function onCalculateCDValueBtn(){
     var depositAmount = Number(document.getElementById("depositAmount").value);
     var interestCDRate = Number(document.getElementById("interestCDRate").value);
     var termCDLength = Number(document.getElementById("termCDLength").value);
     interestCDRate = interestCDRate * 0.01;
 
-    // A = P(1+r/n)(nt)
-    var resultCDValue = depositAmount * Math.pow(1+(interestCDRate/365), (365 * termCDLength));
+    var resultCDValue = depositAmount * (1+interestCDRate/365) ** (365 * termCDLength);
     document.getElementById("futureValue").value = resultCDValue.toFixed(2);
     var resulTotalInterest = resultCDValue - depositAmount;
     document.getElementById("totalInterestEarned").value = resulTotalInterest.toFixed(2);
+}
+
+function onResetCDBtn(){
+    document.getElementById("depositAmount").value = "";
+    document.getElementById("interestCDRate").value = "";
+    document.getElementById("termCDLength").value = "";
+    document.getElementById("futureValue").value = "";
+    document.getElementById("totalInterestEarned").value = "";
 }
 
 function onCalculatePresentValueBtn(){
@@ -57,4 +79,11 @@ function onCalculatePresentValueBtn(){
     var resultPVAmount = paymentAmount*((1-(1+interestPVRate)**-termPay)/interestPVRate);
     document.getElementById("presentValue").value = resultPVAmount.toFixed(2);
     // alert("Hello");
+}
+
+function onResetPVBtn(){
+    document.getElementById("paymentAmount").value = "";
+    document.getElementById("interestPVRate").value = "";
+    document.getElementById("numYears").value = "";
+    document.getElementById("presentValue").value = "";
 }
