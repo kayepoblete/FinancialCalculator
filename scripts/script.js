509fcb327a8d78a10863e10dda7de5c3b365ee36
kayepoblete <93743810+kayepoblete@users.noreply.github.com>
2023-05-01 13:11:05 -0700

    Update script.js
    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -26,17 +26,17 @@ function init(){
 }
 
 function onCalculateMortgageBtn(){
-    var principalAmount = Number(document.getElementById("principalAmount").value);
-    var interestRate = Number(document.getElementById("interestRate").value);
-    var loanLength = Number(document.getElementById("loanLength").value);
+    let principalAmount = Number(document.getElementById("principalAmount").value);
+    let interestRate = Number(document.getElementById("interestRate").value);
+    let loanLength = Number(document.getElementById("loanLength").value);
     // monthly rate
     interestRate = (interestRate*0.01)/12;
     // years to months
     loanLength = loanLength*12;
 
-    var resultMonthly = ((principalAmount*interestRate)*Math.pow((1+interestRate), loanLength))/(Math.pow((1+interestRate), loanLength)-1);
+    let resultMonthly = ((principalAmount*interestRate)*Math.pow((1+interestRate), loanLength))/(Math.pow((1+interestRate), loanLength)-1);
     document.getElementById("monthlyPay").value = resultMonthly.toFixed(2);
-    var resultTotalInt = (resultMonthly*loanLength) - principalAmount;
+    let resultTotalInt = (resultMonthly*loanLength) - principalAmount;
     document.getElementById("totalInterest").value = resultTotalInt.toFixed(2);
 
 }
@@ -50,14 +50,14 @@ function onResetMortgageBtn(){
 }
 
 function onCalculateCDValueBtn(){
-    var depositAmount = Number(document.getElementById("depositAmount").value);
-    var interestCDRate = Number(document.getElementById("interestCDRate").value);
-    var termCDLength = Number(document.getElementById("termCDLength").value);
+    let depositAmount = Number(document.getElementById("depositAmount").value);
+    let interestCDRate = Number(document.getElementById("interestCDRate").value);
+    let termCDLength = Number(document.getElementById("termCDLength").value);
     interestCDRate = interestCDRate * 0.01;
 
-    var resultCDValue = depositAmount * (1+interestCDRate/365) ** (365 * termCDLength);
+    let resultCDValue = depositAmount * (1+interestCDRate/365) ** (365 * termCDLength);
     document.getElementById("futureValue").value = resultCDValue.toFixed(2);
-    var resulTotalInterest = resultCDValue - depositAmount;
+    let resulTotalInterest = resultCDValue - depositAmount;
     document.getElementById("totalInterestEarned").value = resulTotalInterest.toFixed(2);
 }
 
@@ -70,13 +70,13 @@ function onResetCDBtn(){
 }
 
 function onCalculatePresentValueBtn(){
-    var paymentAmount = Number(document.getElementById("paymentAmount").value);
-    var interestPVRate = Number(document.getElementById("interestPVRate").value);
-    var numYears = Number(document.getElementById("numYears").value);
+    let paymentAmount = Number(document.getElementById("paymentAmount").value);
+    let interestPVRate = Number(document.getElementById("interestPVRate").value);
+    let numYears = Number(document.getElementById("numYears").value);
     interestPVRate = (interestPVRate * 0.01)/12;
-    var termPay = numYears*12;
+    let termPay = numYears*12;
 
-    var resultPVAmount = paymentAmount*((1-(1+interestPVRate)**-termPay)/interestPVRate);
+    let resultPVAmount = paymentAmount*((1-(1+interestPVRate)**-termPay)/interestPVRate);
     document.getElementById("presentValue").value = resultPVAmount.toFixed(2);
     // alert("Hello");
 }
